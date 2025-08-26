a8113e99bea4501d4e14a9343576f644dcfaf83a
Mohit Khambekar <mohitkhambekar@gmail.com>
2025-08-26 17:02:46 +0530

    Assignment-3 Uploaded

---
+++ Assignment-3.py
@@ -0,0 +1,28 @@
+import spacy
+from spacy import displacy
+nlp = spacy.load("en_core_web_sm")
+
+example_class_text = (
+    "Elon Musk is the CEO of Tesla."
+    "Golden Gate Bridge is situated in San Francisco."
+    "Donald Trump is the current president of USA."
+    "The GDP of India is 4.19 trillion dollars."
+    "The first manufactured car was made by Ford."
+    "Italy is famous for its Pizza."
+    "The Second World War came to end in 1945. "
+    "Andes are tallest mountain ranges in American continent."
+    "Dead Sea is mostly of salt."
+)
+example_class_doc = nlp(example_class_text)
+
+for ent in example_class_doc.ents:
+    print(
+        f"""
+{ent.text = }
+{ent.start_char = }
+{ent.end_char = }
+{ent.label_ = }
+spacy.explain('{ent.label_}') = {spacy.explain(ent.label_)}"""
+)
+    
+displacy.serve(example_class_doc, style="ent")
